26b5ae1a85b9f6c1db3eba446e3e995295fa59ec
Paula Guillen <paulaguillen.iq@gmail.com>
2022-08-24 20:26:34 -0300

    Primer commit

---
+++ JS/primeraClase.js
@@ -0,0 +1,15 @@
+console.log("Hola mundo");
+document.write("hola mundo");
+
+console.log("este es un 2do mensaje");
+
+const PI= 3.14; 
+console.log(typeof PI)
+
+/*
+let nombre = prompt("Escribe un nombre");
+console.log(nombre);
+
+nombre = "hola";
+console.log(nombre)
+*/
